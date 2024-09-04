244b37101eb29de0ad4767b96d87c06586191c67
KarlaJokic <jokic.karla@gmail.com>
2024-09-04 21:14:45 +0200

    povezivanje na tmdb

--- lib/movie_details_screen.dart
+++ lib/movie_details_screen.dart
@@ -3,15 +3,26 @@ import 'package:flutter/material.dart';
 class MovieDetailsScreen extends StatelessWidget {
   final Map movie;
 
-  const MovieDetailsScreen({super.key, required this.movie});
+  const MovieDetailsScreen({required this.movie, super.key});
 
   @override
   Widget build(BuildContext context) {
+    final posterUrl = 'https://image.tmdb.org/t/p/w500${movie['poster_path']}';
+
     return Scaffold(
       appBar: AppBar(title: Text(movie['title'])),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Text(movie['description']),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.network(posterUrl),  // Dodajemo poster slike
+            const SizedBox(height: 10),
+            Text('Title: ${movie['title']}', style: Theme.of(context).textTheme.headlineSmall),
+            const SizedBox(height: 10),
+            Text('Overview: ${movie['overview']}'),
+          ],
+        ),
       ),
     );
   }
--- lib/movie_list_screen.dart
+++ lib/movie_list_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:dio/dio.dart';
+import 'package:go_router/go_router.dart';
 
 final moviesProvider = FutureProvider<List>((ref) async {
   final response = await Dio().get('https://api.themoviedb.org/3/movie/popular?api_key=faa5bc2acc3d9a811902b1ef84bd6ba8');
@@ -15,13 +16,38 @@ class MovieListScreen extends ConsumerWidget {
     final moviesAsyncValue = ref.watch(moviesProvider);
 
     return Scaffold(
-      appBar: AppBar(title: const Text('Movies')),
+      appBar: AppBar(
+        title: const Text('Movies'),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+      ),
       body: moviesAsyncValue.when(
         data: (movies) => ListView.builder(
           itemCount: movies.length,
-          itemBuilder: (context, index) => ListTile(
-            title: Text(movies[index]['title']),
-          ),
+          itemBuilder: (context, index) {
+            final movie = movies[index];
+            final posterUrl = 'https://image.tmdb.org/t/p/w500${movie['poster_path']}';
+
+            return ListTile(
+              leading: Image.network(
+                posterUrl,
+                width: 50,
+                fit: BoxFit.cover,
+                errorBuilder: (context, error, stackTrace) {
+                  return const Icon(Icons.error); // Ako slika ne uspije učitati
+                },
+              ),
+              title: Text(
+                movie['title'],
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+              subtitle: Text(
+                'Release Date: ${movie['release_date']}', // Dodan podnaslov s datumom izlaska
+                style: TextStyle(color: Colors.grey[600]),
+              ),
+              trailing: const Icon(Icons.arrow_forward_ios), // Dodana strelica za bolji UX
+              onTap: () => context.go('/movies/${movie['id']}', extra: movie),
+            );
+          },
         ),
         loading: () => const Center(child: CircularProgressIndicator()),
         error: (error, stack) => Center(child: Text('Error: $error')),
